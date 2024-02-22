070c60d29ce6dd1590d95fea1858333c1ed12881
dangkhoado43 <dangkhoado43@gmail.com>
2024-02-22 15:54:09 +0700

    update database

--- src/java/db/DBContext.java
+++ src/java/db/DBContext.java
@@ -1,25 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author Tuyen
+ * @author OS
  */
 public class DBContext {
-    
-    public Connection getConnection()throws Exception {
-        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userId, passWord);
+
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        conn = DriverManager.getConnection(url, user, password);
+        return conn;
     }
-    private final String serverName="DESKTOP-AP33H3D";
-    private final String dbName= "DonateDoDreaming";
-    private final String portNumber="1433";
-    private final String instance="";
-    private final String userId="sa";
-    private final String passWord="123456";
+
+    private final String serverName = "localhost";
+    private final String portNumber = "1433";
+    private final String user = "sa";
+    private final String password = "1234";
+    private final String dbName = "DonationMng";
 }

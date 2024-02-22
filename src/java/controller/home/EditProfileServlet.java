1898e7d41d6c552cd906577b1ededec29e3839ed
dangkhoado43 <dangkhoado43@gmail.com>
2024-02-22 19:49:56 +0700

    create home servlet

--- src/java/controller/home/EditProfileServlet.java
+++ src/java/controller/home/EditProfileServlet.java
@@ -1,21 +1,15 @@
 package controller.home;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import dao.DAO;
 import model.User;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.sql.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
